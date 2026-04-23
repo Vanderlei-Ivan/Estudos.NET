fd66347fac4bca1015fcf58603d7e227ab0cda99
Vanderlei Ivan <db1.vanderlei.reis@parceirocantu.com.br>
2026-04-23 11:16:51 -0300

    feat: tetando envio

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,6 +11,6 @@ public class Customer
     public string? Name { get; set;}
     public string? LastName { get; set;}
     public string? Email { get; set;}
-    public decimal Phone { get; set;}
+    public string? Phone { get; set;}
     public Address? Adress { get; set;}
 }
